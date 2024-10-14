bb3470773ca4d0bfd5aec30b96aa8416f0b72b73
oil3 <3oiltree@gmail.com>
2024-10-13 20:38:08 -0500

    Initial Commit

---
+++ CodeColors/CodeColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeColorsApp.swift
+//  CodeColors
+//
+//  Created by Almahdi Morris on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
